6bcaba15ad4498ac747ba392fbed7e9378534b84
Mark <mark.bin.lu@Gmail.com>
2024-08-18 15:28:48 +1000

    added images

--- main.py
+++ main.py
@@ -9,10 +9,24 @@ training_images, testing_images = training_images / 255.0, testing_images / 255.
 class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
 for i in range(16):
-    plt.subplot(4,4, i+1)
+    plt.subplot(4, 4, i+1)
     plt.xticks([])
     plt.yticks([])
     plt.imshow(training_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[training_labels[i][0]])
 
 plt.show()
+#
+
+#
+model = models.load_model('/Users/mark/PycharmProjects/image_classification/model.classifier.keras')
+
+img = cv.imread('deer.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+prediction = model.predict(np.array([img]) / 255)
+index = np.argmax(prediction)
+print(f'Prediction is : {class_names[index]}')
+
+plt.show
