f540a1df6d3b38f632ed35b722cf569ff9618afe
Mark <mark.bin.lu@Gmail.com>
2024-08-18 15:32:47 +1000

    e

--- main.py
+++ main.py
@@ -16,17 +16,42 @@ for i in range(16):
     plt.xlabel(class_names[training_labels[i][0]])
 
 plt.show()
-#
 
 #
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10, activation='softmax'))
+
+# convolutional layer filters for features in an image.
+# For example, it will look at okay a horse has long legs
+# whereas a cat has pointy ears
+
+# The maxpooling then removes features of an image to its essential parts
+
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# model.fit(training_images, training_labels)
+
+# model.fit(training_images, training_labels, epochs=20, validation_data=(testing_images, testing_labels))
+#
+# loss, accuracy = model.evaluate(testing_images, testing_labels)
+# print(f"Loss: {loss}")
+# print(f"Accuracy: {accuracy}")
+#
+# model.save('model.classifier.model')
+
 model = models.load_model('/Users/mark/PycharmProjects/image_classification/model.classifier.keras')
 
-img = cv.imread('deer.jpg')
+img = cv.imread('horse.jpg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 plt.imshow(img, cmap=plt.cm.binary)
 prediction = model.predict(np.array([img]) / 255)
 index = np.argmax(prediction)
 print(f'Prediction is : {class_names[index]}')
-
-plt.show
